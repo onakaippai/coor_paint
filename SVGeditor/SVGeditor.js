b54a7d7d60eaebb61979a9ee05cb0c5128358cbe
onakaippai <josinrin@gmail.com>
2020-10-30 00:11:03 +0900

    get/set caret position

--- SVGeditor/SVGeditor.js
+++ SVGeditor/SVGeditor.js
@@ -23,11 +23,15 @@ function GrfTabClk(TabBnt, GrfVwr){
 	TabBnt.style.borderBottomColor = "Gainsboro";
 	GrfVwr.style.display = "block";
 }
+function DrawClk(){
+}
+function KeepClk(){
+}
 function SetupClk(){
 	var GrfWth, GrfHgt, ZomPct;
-	GrfWth = parseInt(GrfTabObj.children[0].children[0].textContent, 10);
-	GrfHgt = parseInt(GrfTabObj.children[0].children[1].textContent, 10);
-	ZomPct = parseInt(GrfTabObj.children[1].children[0].textContent, 10);
+	GrfWth = parseInt(GrfTabObj.children[0].children[0].value, 10);
+	GrfHgt = parseInt(GrfTabObj.children[0].children[1].value, 10);
+	ZomPct = parseInt(GrfTabObj.children[1].children[0].value, 10);
 	TtlSvgObj.setAttribute("width", String(GrfWth));
 	TtlSvgObj.setAttribute("height", String(GrfHgt));
 	TtlSvgObj.style.marginTop = String((480 - GrfHgt) / 2) + "px";
@@ -36,6 +40,77 @@ function SetupClk(){
 	CurCnvObj.style.marginTop = String((480 - GrfHgt) / 2) + "px";
 	CurCnvObj.width = String(GrfWth);
 	CurCnvObj.height = String(GrfHgt);
+}
+function getCaretCharacterOffsetWithin(element) {
+    var caretOffset = 0;
+    var doc = element.ownerDocument || element.document;
+    var win = doc.defaultView || doc.parentWindow;
+    var sel;
+    if (typeof win.getSelection != "undefined") {
+        sel = win.getSelection();
+        if (sel.rangeCount > 0) {
+            var range = win.getSelection().getRangeAt(0);
+            var preCaretRange = range.cloneRange();
+            preCaretRange.selectNodeContents(element);
+            preCaretRange.setEnd(range.endContainer, range.endOffset);
+            caretOffset = preCaretRange.toString().length;
+        }
+    } else if ((sel = doc.selection) && sel.type != "Control") {
+        var textRange = sel.createRange();
+        var preCaretTextRange = doc.body.createTextRange();
+        preCaretTextRange.moveToElementText(element);
+        preCaretTextRange.setEndPoint("EndToEnd", textRange);
+        caretOffset = preCaretTextRange.text.length;
+    }
+    return caretOffset;
+}
+
+function setCaretPosition(element, offset) {
+    var range = document.createRange();
+    var sel = window.getSelection();
+
+    //select appropriate node
+    var currentNode = null;
+    var previousNode = null;
+
+    for (var i = 0; i < element.childNodes.length; i++) {
+        //save previous node
+        previousNode = currentNode;
+
+        //get current node
+        currentNode = element.childNodes[i];
+        //if we get span or something else then we should get child node
+       while(currentNode.childNodes.length > 0){
+          currentNode = currentNode.childNodes[0];
+       }
+
+        //calc offset in current node
+        if (previousNode != null) {
+            offset -= previousNode.length;
+        }
+        //check whether current node has enough length
+        if (offset <= currentNode.length) {
+            break;
+        }
+    }
+    //move caret to specified offset
+    if (currentNode != null) {
+        range.setStart(currentNode, offset);
+        range.collapse(true);
+        sel.removeAllRanges();
+        sel.addRange(range);
+    }
+}
+function CurPthChange(CdeEdt){
+    //var Info, Flag, Coor;    
+    //Info = ExtractInfo(CdeEdt.innerHTML);
+    //Flag = FormatCode(CdeEdt.innerHTML, info);
+    //if (Flag){
+    //    Coor = ExtractCoor(Info);
+    //}
+    var position = getCaretCharacterOffsetWithin(CdeEdt);
+    CdeEdt.innerHTML = "<span style=\"color:blue\">"+CdeEdt.textContent+"</span>";    
+    setCaretPosition(CdeEdt, position);
 }
 function PreLoad(){
 	CdeTabObj = document.getElementById("CdeTab");
@@ -56,7 +131,13 @@ function PreLoad(){
 	GrfTabObj.children[3].addEventListener("click", function(){
 		GrfTabClk(GrfTabObj.children[3], TtlSvgObj);
 	}, false);
-	GrfTabObj.children[4].addEventListener("click", SetupClk, false);
+	CurPthObj.addEventListener("keyup", function(){
+		CurPthChange(CurPthObj);
+	}, false);
+	//CdeTabObj.children[2].addEventListener("click", DrawClk, false);    
+	//CdeTabObj.children[3].addEventListener("click", KeepClk, false);
+    GrfTabObj.children[4].addEventListener("click", SetupClk, false);
+    //CurPthObj.children[0].addEventListener("change", CurPthChange, false);
 	CdeTabClk(CdeTabObj.children[0], CurPthObj);
 	GrfTabClk(GrfTabObj.children[2], CurCnvObj);
 	SetupClk();
